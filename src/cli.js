4151e63b0ad7d9856d9607be002ed28bf773fcdb
Nicolas Mérouze <nicolas.merouze@gmail.com>
2016-01-29 12:48:33 +0900

    Fix CLI.

--- src/cli.js
+++ src/cli.js
@@ -5,12 +5,14 @@ program
   .version('0.1.0');
 
 program
-  .command('start <path>', { isDefault: true })
+  .command('start <path>')
   .description('start web server')
   .option('-p, --port <n>', 'web server port (default: 3000)', parseInt)
   .action(function(projectsPath, options){
-    process.env.PORT = options.port;
-    
+    if (options.port !== undefined) {
+      process.env.PORT = options.port;
+    }
+
     if (path.isAbsolute(projectsPath)) {
       process.env.PROJECTS_DIR = projectsPath;
     } else {
