64a878302217b9da03a07bd50f14880c02a4df51
Nicolas Mérouze <nicolas.merouze@gmail.com>
2016-02-18 11:31:56 +0900

    Fix setup.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -16,40 +16,40 @@ module.exports = function(projectsPath) {
 
     fs.writeFileSync(path.join(projectsPath, 'index.js'), `const secretKey = '${buf.toString('hex')}';
 
-  // Replace localhost by a real domain name in production
-  // You can use Pow or Anvil to get multiple domain names on a development machine
-  module.exports = {
-    newProject: {
-      domains: ['localhost'],
-      secretKey: secretKey
-    }
-  };
-    `);
+// Replace localhost by a real domain name in production
+// You can use Pow or Anvil to get multiple domain names on a development machine
+module.exports = {
+  newProject: {
+    domains: ['localhost'],
+    secretKey: secretKey
+  }
+};
+`);
   });
 
   fs.writeFileSync(path.join(newProjectPath, 'content', 'links.csv'), `url,description
-  https://www.nicolasmerouze.com/mikan,Homepage of Mikan CMS
-  http://localhost:3000/a/links,Link to access these links
-  `);
+https://www.nicolasmerouze.com/mikan,Homepage of Mikan CMS
+http://localhost:3000/a/links,Link to access these links
+`);
 
   fs.mkdirSync(path.join(newProjectPath, 'content', 'posts'));
-  fs.writeFileSync(path.join(newProjectPath, 'content', 'hello-world.md'), `---
-  title: Hello World
-  ---
+  fs.writeFileSync(path.join(newProjectPath, 'content', 'posts', 'hello-world.md'), `---
+title: Hello World
+---
 
-  This is a hello world post. You can access the posts collection with endpoint http://localhost:3000/a/posts.
-  `);
+This is a hello world post. You can access the posts collection with endpoint http://localhost:3000/a/posts.
+`);
 
   fs.writeFileSync(path.join(newProjectPath, 'users', 'admin.md'), `---
-  email: admin@example.com
-  password:
-  roles: [admin]
-  ---
+email: admin@example.com
+password:
+roles: [admin]
+---
 
-  User biography goes here.
-  `);
+User biography goes here.
+`);
 
   fs.writeFileSync(path.join(newProjectPath, 'static', 'test.txt'), `
-  You can access this file with the following URL: http://localhost:3000/test.txt
-  `);
+You can access this file with the following URL: http://localhost:3000/test.txt
+`);
 }
